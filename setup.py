85e848d2b994b6f8358046392af22e4ea997f6bf
Samuel-Enyaude <enyaudesamuel@hotmail.com>
2024-09-17 13:51:24 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='California_House_Price_Prediction',
+    version='0.1',
+    license='MIT',
+    description='California House Price Prediction Application ',
+    install_requires=['SQL', 'Numpy', 'Pandas','Scitkit_learn', 'JupyterNotebook'],
+    url='https://github.com/Enyaude/California_House_Price_Prediction.git',
+    author='<Enyaude>',
+    author_email='<enyaudesamuel@hotmail.com>'
+    )
